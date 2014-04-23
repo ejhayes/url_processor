3eb627c87d33ed4165fef492052afe81ea07957b
Eric Hayes <eric@deployfx.com>
2014-04-23 15:54:23 -0700

    Added main module functionality.

--- lib/url_processor.rb
+++ lib/url_processor.rb
@@ -1,5 +1,24 @@
-require "url_processor/version"
+require 'typhoeus'
+require 'colorize'
+require 'ostruct'
+require 'url_processor/version'
+require 'url_processor/error'
+require 'url_processor/runner'
+require 'url_processor/config'
+require 'url_processor/base'
+require 'url_processor/retryable_request'
+require 'url_processor/link_request'
 
 module UrlProcessor
-  # Your code goes here...
+
+  def self.create(&block)
+    config = Config.new
+    block.call config
+
+    # validate before returning
+    config.validate!
+
+    Runner.new(config)
+  end
+
 end
--- spec/url_processor_spec.rb
+++ spec/url_processor_spec.rb
@@ -1,11 +1,36 @@
 require 'spec_helper'
 
 describe UrlProcessor do
-  it 'should have a version number' do
-    UrlProcessor::VERSION.should_not be_nil
+
+  describe :gem_attributes do
+    it 'should have a version number' do
+      UrlProcessor::VERSION.should_not be_nil
+    end
   end
 
-  it 'should do something useful' do
-    false.should eq(true)
+  describe '.create' do
+
+    it 'requires a block with a config argument' do
+      UrlProcessor::Config.any_instance.stub(:validate!)
+      UrlProcessor::Config.any_instance.should_receive(:some_random_call)
+
+      UrlProcessor.create do |config|
+        config.some_random_call
+      end
+    end
+
+    it 'throws an error if a block is not passed' do
+      expect { UrlProcessor.create }.to raise_error NoMethodError
+    end
+
+    it 'returns a runner' do
+      processor = UrlProcessor.create do |config|
+        config.stub(:validate!)
+      end
+
+      processor.should be_a UrlProcessor::Runner
+    end
+
   end
+
 end
